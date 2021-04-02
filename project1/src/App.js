3a82157a561aaaf1f103dfe0d92611a5332bce23
Thamer Saraei <tsarei@legroupejsv.onmicrosoft.com>
2021-04-02 18:13:58 -0400

    first component

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-             <p> First Component </p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (
+      <div className="App">
+          <p> First Component </p>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
